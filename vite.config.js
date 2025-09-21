797883dd7d9d10c8fb8e678cd928742120dc03e3
Tim Huang <tth8701@Tims-MacBook-Pro-2.local>
2025-09-20 21:07:09 -0400

    reconfigure homepage url in package.json and base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: process.env.NODE_ENV === 'production' ? '/picktopia_website/' : '/',
+	base: '/',
 	assetsInclude: ['**/*.MP4', '**/*.mp4']
 })
